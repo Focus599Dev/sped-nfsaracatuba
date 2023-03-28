018f588de62915b65942d8d3214c476bef128ac2
edusatelis <edusatelis@gmail.com>
2023-03-28 11:15:26 -0300

    Ajustando tls para 1.3

--- src/Soap/Soap.php
+++ src/Soap/Soap.php
@@ -33,7 +33,7 @@ class Soap
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
             // curl_setopt($ch, CURLOPT_IPRESOLVE, CURLOPT_IPRESOLVE_V4);
-            curl_setopt($ch, CURLOPT_SSLVERSION, 4);
+            curl_setopt($ch, CURLOPT_SSLVERSION, 7);
             curl_setopt($ch, CURLOPT_URL, $soapUrl);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
